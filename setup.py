c74742358313837f239d0083113e634cf61e08ff
eldhomundackal <eldhobabym@gmail.com>
2022-05-05 18:31:51 +0530

    Add tox

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name ='form-service',
+    author='Eldho',
+    url='https://github.com/eldhomundackal/form-services',
+    packages= find_packages()
+
+)
